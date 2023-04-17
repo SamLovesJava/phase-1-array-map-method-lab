af1f194f4fd61ada326a8d4d6bf0ed9e2126fbb7
Hosam H <harooubi@gmail.com>
2023-04-16 22:01:48 -0400

    done

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(
+    line => line.split(" ").map(
+      word => word[0].toUpperCase() + word.slice(1)
+    ).join(" ")
+  )
 }
